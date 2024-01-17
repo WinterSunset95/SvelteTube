5e02b689ef34c1e551912c73b9fd4ffba704f4fb
WinterSunset95 <wintersunset95@gmail.com>
2024-01-17 12:45:52 +0530

    Testing breaking changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-import { wserver } from './src/socket';
+import { wserver } from './server/socket'
 
 export default defineConfig({
 	plugins: [
