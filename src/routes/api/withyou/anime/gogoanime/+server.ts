0bef96973176da6479815e4a34ec26f9719cc7b1
WinterSunset95 <wintersunset95@gmail.com>
2024-06-06 16:55:12 +0530

    added paging on search page

--- src/routes/api/withyou/anime/gogoanime/+server.ts
+++ src/routes/api/withyou/anime/gogoanime/+server.ts
@@ -4,11 +4,12 @@ const gogo = new ANIME.Gogoanime();
 
 export async function GET({ fetch, url }) {
 	let search = url.searchParams.get("search");
+	let page = url.searchParams.get("page");
 
 	let res: ISearch<IAnimeResult>
 
 	try {
-		res = await gogo.search(search);
+		res = await gogo.search(search, page ? parseInt(page) : 1);
 	} catch (e) {
 		return new Response(JSON.stringify(e))
 	}
