1e1b7b4d825412605ee9278678606e8c9965231a
WinterSunst95 <wintersunset95@gmail.com>
2026-08-18 12:48:43 +0530

    feat: nixified the dev environment

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "SvelteTube";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_22
+          corepack_22
+
+          svelte-language-server
+          typescript-language-server
+          tailwindcss-language-server
+          vscode-langservers-extracted
+        ];
+
+        shellHook = ''
+          echo "SvelteTube Dev Environment Loaded";
+          echo "Node version: $(node --version)
+
+          corepack enable pnpm
+        '';
+      };
+    }
+  );
+}
